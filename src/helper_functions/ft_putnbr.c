1b80e98efeb6e83b33d6daa962b20a2989c8eba2
Gina Bartusch <170827275+gbartusch@users.noreply.github.com>
2024-10-27 10:56:39 +0100

    minor improvements

--- src/helper_functions/ft_putnbr.c
+++ src/helper_functions/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: gbartusc <gbartusc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 15:33:43 by gbartusc          #+#    #+#             */
-/*   Updated: 2024/10/26 15:43:42 by gbartusc         ###   ########.fr       */
+/*   Updated: 2024/10/27 09:41:15 by gbartusc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,20 @@ int	ft_putnbr(int n)
 	if (n > 9)
 	{
 		count += ft_putnbr(n / 10);
+	}
+	ft_putchar((n % 10) + '0');
+	count++;
+	return (count);
+}
+
+int	ft_putnbr_unsigned(unsigned int n)
+{
+	int	count;
+
+	count = 0;
+	if (n > 9)
+	{
+		count += ft_putnbr_unsigned(n / 10);
 	}
 	ft_putchar((n % 10) + '0');
 	count++;
--- src/helper_functions/ft_putstr.c
+++ src/helper_functions/ft_putstr.c
@@ -6,34 +6,33 @@
 /*   By: gbartusc <gbartusc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 15:33:50 by gbartusc          #+#    #+#             */
-/*   Updated: 2024/10/25 16:01:36 by gbartusc         ###   ########.fr       */
+/*   Updated: 2024/10/27 10:16:08 by gbartusc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return(i);
+}
+
 int	ft_putstr(char *str)
 {
 	int		count;
-	char	*null_str;
 
 	count = 0;
 	if (!str)
 	{
-		null_str = "(null)";
-		while (*null_str)
-		{
-			ft_putchar(*null_str);
-			null_str++;
-			count++;
-		}
+		write (1, "(null)", 7);
+		count += 6;
 		return (count);
 	}
-	while (*str != '\0')
-	{
-		ft_putchar(*str);
-		str++;
-		count++;
-	}
-	return (count);
+	write (1, str, ft_strlen(str));
+	return (ft_strlen(str));
 }
